eca45278c5089813f038f37b3341dfa4833def90
Bryce Henderson <brycehenderson05@gmail.com>
2026-01-09 04:12:15 -0600

    Added basic structure of quiz with a sample question

--- quiz_game.py
+++ quiz_game.py
@@ -5,3 +5,35 @@ class Question:
         self.text = text
         self.choices = choices
         self.answer = answer
+
+
+
+class Quiz:
+    def __init__(self, name):
+        self.name = name
+        self.questions = []
+            
+    def addQuestion(self, q):
+        self.questions.append(q)
+
+    def runQuestion(self):
+            q = self.questions[0]
+            print(q.text)
+            for choice in q.choices:
+                 print(choice)
+            guess = input()
+            if (guess == q.answer):
+                 print("Correct!")
+            else: print("Wrong!")
+
+
+
+
+myQuiz = Quiz("Capitals")
+q1text = "What is the Capital of Oklahoma?"
+q1choices = ["Austin", "Oklahoma City", "Little Rock", "Tulsa"]
+q1answer = "Oklahoma City"
+
+q1 = Question(q1text, q1choices, q1answer)
+myQuiz.addQuestion(q1)
+myQuiz.runQuestion()
